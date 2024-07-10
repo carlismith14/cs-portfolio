647140f2a1003d032c57226c32e51247da509c3e
Carli <carli.t.smith1@gmail.com>
2024-07-10 17:25:01 -0400

    updated all project content

--- src/pages/Home/ProjectData.js
+++ src/pages/Home/ProjectData.js
@@ -1,10 +1,11 @@
 export const projectOne = [
-
   {
     title: "eProcess Development",
-    subtitle: "compliant website",
+    subtitle: "optimized website overhaul",
 
     description: "Responsive site developed with Next.js and tailwind CSS",
+    description:
+      "Business website optimized for accessibility, load time, & seo. Built with Next.js and tailwind CSS and deployed via AWS Amplify.",
     lightText: false,
     img: require("../../images/eProcess2.png"),
     link: "eprocess",
@@ -21,17 +22,20 @@ export const projectOne = [
       },
 
       intro:
-        "Revamping the business website for the company I've been contracted with offered an insightful journey into website redesign. Identifying critical shortcomings in their previous site laid the groundwork for a comprehensive overhaul. Leveraging Next.js, we prioritized optimizing the new site for page load efficiency, accessibility, and SEO performance. This project not only addressed immediate needs but also laid a solid foundation for eProcess Development’s digital presence moving forward.",
+        // "Revamping the business website for the company I've been contracted with offered an insightful journey into website redesign. Identifying critical shortcomings in their previous site laid the groundwork for a comprehensive overhaul. Leveraging Next.js, we prioritized optimizing the new site for page load efficiency, accessibility, and SEO performance. This project not only addressed immediate needs but also laid a solid foundation for eProcess Development’s digital presence moving forward.",
+        "Revamping the business website for my contracted company involved identifying critical shortcomings and executing a comprehensive overhaul in both design and development. Using Next.js, we optimized the site for page load efficiency, accessibility, and SEO, laying a solid foundation for eProcess Development’s digital presence.",
       role: "Developer & Web Designer",
       techStack: ["Next.js", "TailwindCSS", "Nodemailer", "AWS"],
       challenges: [
-        "Implementation of Contact Form",
         "Establishing Site Infastructure & Framework",
+        "Implementation of Contact Form",
         "Optimizing towards Performance, Accessibility & SEO Performance",
-        "Deployment via AWS",
+        "Deployment via AWS Amplify",
       ],
       lessons:
-        "The process of rebuilding our company's website provided invaluable lessons, particularly in three key areas. Firstly, adopting Next.js revealed its transformative power in simplifying development workflows and optimizing performance. Secondly, embracing Tailwind CSS showcased its remarkable ease and flexibility, enabling rapid prototyping and seamless design adjustments. Lastly, navigating the deployment process on AWS deepened my understanding of cloud infrastructure management, emphasizing the importance of scalability and reliability. These experiences underscored the significance of leveraging the right technologies to achieve efficient, flexible, and robust web solutions.",
+        // " Adopting Next.js revealed its transformative power in simplifying development workflows and optimizing performance.  Tailwind CSS showcased its remarkable ease and flexibility, enabling rapid prototyping and seamless design adjustments. Lastly, navigating the deployment process on AWS Amplify. ",
+
+        "Adopting Next.js taught me how it can simplify development workflows and optimize performance. I found Tailwind CSS to be incredibly flexible, which made rapid prototyping and design adjustments much easier. Navigating the deployment process on AWS Amplify also gave me valuable experience in efficient and scalable deployment practices.",
       note: false,
       noteContext: "",
       githubLink: "https://github.com/kurtrodenhizer/epd2024",
@@ -44,7 +48,8 @@ export const projectOne = [
     subtitle: "asset management tool & dashboard",
 
     description:
-      "Interactive asset management dashboard with advanced Sort & Filter capabilities. Built with React and Material UI components ",
+      // "Interactive asset management dashboard with advanced Sort & Filter capabilities. Built with React and Material UI components ",
+      "Interactive asset management dashboard with advanced sort and filter capabilities, built with React and Material UI components. Utilizes APIs for dynamic data fetching and display.",
     lightText: false,
     img: require("../../images/faris_project_mainimage.png"),
     link: "/cmsdashboard",
@@ -54,21 +59,18 @@ export const projectOne = [
     projectInfo: {
       media: {
         img: true,
-        imgSrc: require("../../images/dashboard.png"),
+        // imgSrc: require("../../images/dashboard.png"),
+        imgSrc: require("../../images/faris_project_mainimage.png"),
+
         vid: false,
         vidSrc: null,
       },
 
       intro:
-        "The File Asset Resource Intelligence System (FARIS) is a centralized hub for storing and sharing essential business assets. As the front-end developer, I transformed Figma designs into a functional interface and collaborated with back-end developers to integrate all relevant assets and data. For this demo, I chose to highlight the most challenging feature: the “recently shared” dashboard. This demo focuses on this specific feature and does not reflect all the work done on the entire project. I used Mockaroo to generate a mock dataset, demonstrating the dashboard’s capabilities, including filtering and sorting of recently shared assets.",
-      role: "Frontend Development",
-      techStack: [
-        "React",
-        "Material UI",
-        "APIs",
-      ],
+        "The File Asset Resource Intelligence System (FARIS) is a centralized hub for storing and sharing business assets. As the front-end developer, I brought Figma designs to life and collaborated with back-end developers to integrate all relevant data. For this demo, I focused on the “recently shared” dashboard, and used Mockaroo's API to generate an anonymized data set.",
+      role: "Frontend Developer",
+      techStack: ["React", "Material UI", "API"],
       challenges: [
-        "Material UI Compoents",
         "Advanced Table Features ",
         "API Integration & Data Handling",
         "Code Refactoring",
@@ -78,8 +80,8 @@ export const projectOne = [
       note: true,
       // noteContext:
       //   "*Please note that the demo link and source code for this project are not available due to privacy concerns. However, you are welcome to view screenshots and read the project writeup on the GitHub repository.",
-      githubLink: "#",
-      demoLink: "#",
+      githubLink: "https://github.com/carlismith14/farisdashboard",
+      demoLink: "https://faris-dashboard.netlify.app/",
     },
   },
 
@@ -88,7 +90,8 @@ export const projectOne = [
     subtitle: "telemetry device simulation",
 
     description:
-      "Functional hybrid application simulating Philips IntelliVue Telemetry Device ",
+      // "Functional hybrid application simulating Philips IntelliVue Telemetry Device ",
+      "Hybrid mobile/web app built with React to simulate Philips’s IntelliVue telemetry device for effective medical staff training and device practice.",
     img: require("../../images/projects/philips/philipsprojectimg1.png"),
     alt: "Image",
     // link:"/work",
@@ -104,6 +107,7 @@ export const projectOne = [
 
       intro:
         "The IntelliVue Sim File, built with React, serves as a versatile hybrid mobile/web application. It mirrors the form, function, and user experience of Philips's portable patient monitoring system. The goal was to create a user-friendly and accessible tool for nurses and hospital staff. This application allows them to become familiar with the monitor's operation using their mobile devices, simplifying their workflow and enhancing convenience.",
+
       role: "Sole Developer & Project Manager",
       techStack: [
         "React",
@@ -131,7 +135,10 @@ export const projectOne = [
   {
     title: "Ai Chatbot",
     subtitle: "interactive OpenAi application",
-    description: "Interactive web application with OpenAi integration",
+    // description: "Interactive web application with OpenAi integration",
+    description:
+      "Interactive OpenAI-based chatbot app developed with Next.js. Combines AI with proprietary data for a company-wide training tool.",
+
     // img: require("../../images/projects/philips/philipsprojectimg1.png"),
     img: require("../../images/projects/aichatbot/Screenshot 2023-10-05 at 10.53.59 AM.png"),
 
@@ -149,7 +156,7 @@ export const projectOne = [
       intro:
         "The AI Chatbot combines the power of AI with proprietary product information to act as a company-wide training tool. One of the key highlights of this project was the collaborative efforts between front-end and back-end engineers. Together, we developed an interactive application that leverages OpenAI and offers a seamless user experience.",
       role: "Front-end Developer",
-      techStack: ["Next.js", "Typescript", "OpenAi", "Github"],
+      techStack: ["Next.js", "TailwindCSS", "Typescript", "OpenAi", "Github"],
       challenges: [
         "Responsive Design",
         "User Authorization",
@@ -168,7 +175,9 @@ export const projectOne = [
     title: "Punta Cana",
     subtitle: "responsive restaurant website",
 
-    description: "Static site developed using HTML, CSS & JavaScript",
+    // description: "Static site developed using HTML, CSS & JavaScript",
+    description:
+      "Responsive restaurant website project demonstrating HTML, CSS, and JavaScript skills. Focused on responsive design challenges like navigation and menu display.",
     img: require("../../images/pc.png"),
     alt: "Image",
     link: "/puntacana",
@@ -182,8 +191,11 @@ export const projectOne = [
         vid: false,
         vidSrc: null,
       },
+      role: "Developer & Designer",
+
       intro:
-        "The Punta Cana website was my first real-world development project and allowed me to showcase my skills in HTML, CSS and Javascript, particularly in responsive design.",
+        // "The Punta Cana website was my first real-world development project and allowed me to showcase my skills in HTML, CSS and Javascript, particularly in responsive design.",
+        "For my first website project, Punta Cana, I served as both designer and developer, discovering my passion for coding and clarifying my career aspirations in web development. I used HTML, CSS, and JavaScript to build a responsive restaurant site with features like a navigation bar, CSS Grid menu, and fixed background effects.",
       techStack: ["HTML5", "CSS", "JavaScript", "Netlify"],
       challenges: [
         "Responsive Navigation Bar",
@@ -192,7 +204,9 @@ export const projectOne = [
         "BEM Naming Conventions",
       ],
       lessons:
-        "The Punta Cana restaurant website project was an enlightening experience that left me with valuable insights. As both the designer and developer of the project, I discovered my innate passion for coding, which ultimately clarified my career aspirations in becoming a developer. The project also highlighted the importance of organizational practices, such as BEM naming conventions, to enhance code clarity. While the site was constructed using raw HTML, CSS, and JavaScript, the increasing complexity of the project made me realize the need for frameworks to streamline and improve code management.",
+        // " As both the designer and developer of the project, I discovered my innate passion for coding, which ultimately clarified my career aspirations in becoming a developer. The project also highlighted the importance of organizational practices, such as BEM naming conventions, to enhance code clarity. While the site was constructed using raw HTML, CSS, and JavaScript, the increasing complexity of the project made me realize the need for frameworks to streamline and improve code management.",
+        "This project taught me the importance of organizational practices like BEM naming conventions for clear and maintainable code. Although I built the site with raw HTML, CSS, and JavaScript, the project’s complexity highlighted the need for frameworks to improve code management and efficiency.",
+
       note: false,
       noteContext: "",
       githubLink: "https://github.com/carlismith14/puntacana",
